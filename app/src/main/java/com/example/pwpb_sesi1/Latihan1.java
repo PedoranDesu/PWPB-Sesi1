8fc8eb6a58d0c5a60bc62fc02d5a2c2f8d26ed21
alvarodwi <varoalvaro493@gmail.com>
2019-07-22 11:47:08 +0700

    Update as of 22/7/2019

---
+++ app/src/main/java/com/example/pwpb_sesi1/Latihan1.java
@@ -0,0 +1,14 @@
+package com.example.pwpb_sesi1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Latihan1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_latihan1);
+    }
+}
--- app/src/main/java/com/example/pwpb_sesi1/MainActivity.java
+++ app/src/main/java/com/example/pwpb_sesi1/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.pwpb_sesi1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button but_lat1;
+    Button but_lat2;
+    Button but_tulat1;
+    Button but_tulat2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        but_lat1 = (Button) findViewById(R.id.btn_lat1);
+        but_lat2 = (Button) findViewById(R.id.btn_lat2);
+        but_tulat1 = (Button) findViewById(R.id.btn_tulat1);
+        but_tulat2 = (Button) findViewById(R.id.btn_tulat2);
+
+        but_lat1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(MainActivity.this,LinearLayout.class);
+                startActivity(pindah);
+            }
+        });
+
+        but_lat2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(MainActivity.this,RelativeLayout.class);
+                startActivity(pindah);
+            }
+        });
+
+        but_tulat1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(MainActivity.this,Latihan1.class);
+                startActivity(pindah);
+            }
+        });
+
+        but_tulat2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(MainActivity.this,activity_latihan2.class);
+                startActivity(pindah);
+            }
+        });
     }
 }
